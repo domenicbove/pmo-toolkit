abdab86886638c9c6bcf1d697d4e4511971cc12e
Domenic <domenicbove@gmail.com>
2016-12-08 10:05:53 -0800

    unused import

--- api/src/main/java/com/rhc/pmo/api/Application.java
+++ api/src/main/java/com/rhc/pmo/api/Application.java
@@ -5,6 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
+  
+//  @Bean
+//  String myBean() {
+//      return "I'm Spring bean! RocketChat Status is: " +
+//          rocketChatService.getRCStatus() ;
+//  }
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
